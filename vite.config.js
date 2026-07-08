a034db1636539b2135943c409c24d94ec5e3b695
pagarwal884 <pagarwal1145@gmail.com>
2026-07-08 13:35:41 +0530

    Complete the file setup for the project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname} from "path"
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'Components'),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'constants'),
+      '#store': resolve(dirname(fileURLToPath(import.meta.url)), 'store'),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'hoc'),
+      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), 'windows'),
+    }
+  }
 })

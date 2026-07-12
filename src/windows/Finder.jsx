0f8188386599625c53874ca83680b4e292bda168
pagarwal884 <pagarwal1145@gmail.com>
2026-07-13 00:17:50 +0530

    Added logic to open other window and files from this finder window

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -6,8 +6,10 @@ import WindowControls from "#/Components/WindowControls";
 import WindowWrapper from "#/hoc/WindowWrapper";
 import { locations } from "#/constants";
 import useLocationStore from "#/store/Location";
+import useWindowStore from "#/store/window";
 
 const Finder = () => {
+  const { openWindow } = useWindowStore();
   const { activeLocation, setActiveLocation } = useLocationStore();
 
   const renderLocationList = (items) =>
@@ -26,11 +28,26 @@ const Finder = () => {
           className="w-4 h-4 object-contain"
         />
 
-        <p className="text-sm font-medium truncate">
-          {item.name}
-        </p>
+        <p className="text-sm font-medium truncate">{item.name}</p>
       </li>
     ));
+
+  const openItem = (item) => {
+    if (!item) return;
+
+    switch (item.fileType) {
+      case "folder":
+        setActiveLocation(item);
+        break;
+
+      case "pdf":
+        openWindow("resume");
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <>
@@ -44,19 +61,28 @@ const Finder = () => {
           <div>
             <h3>Favourite</h3>
 
-            <ul>
-              {renderLocationList(Object.values(locations))}
-            </ul>
+            <ul>{renderLocationList(Object.values(locations))}</ul>
           </div>
 
           <div>
-            <h3>Work</h3>
+            <h3>My Projects</h3>
 
-            <ul>
-              {renderLocationList(locations.work.children)}
-            </ul>
+            <ul>{renderLocationList(locations.work.children)}</ul>
           </div>
         </div>
+
+        <ul className="content">
+          {(activeLocation?.children ?? []).map((item) => (
+            <li
+              key={item.id}
+              className={item.position}
+              onClick={() => openItem(item)}
+            >
+              <img src={item.icon} alt={item.name} className="cursor-pointer"/>
+              <p className="cursor-pointer">{item.name}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );

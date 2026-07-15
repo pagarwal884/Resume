a671ff8fc501fc3615dfa16db46152821fc60da6
pagarwal884 <pagarwal1145@gmail.com>
2026-07-15 12:02:05 +0530

    Added Contact window in my site

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -1,11 +1,43 @@
+import WindowControls from '#/Components/WindowControls';
+import { socials } from '#/constants';
+import WindowWrapper from '#/hoc/WindowWrapper';
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="contact"/>
+                <h2>Contact Me</h2>
+            </div>
+
+            <div className="p-5 space-y-5">
+                <img src="/images/adrian.jpg" alt="" className='w-20 rounded-full' />
+                <h3>Let's connect</h3>
+                <p>Got an /idea? A bug to squash? Or just wanna talk tech? I'm in.</p>
+                <p>pagarwal1145@gmail.com</p>
+
+                <ul>
+                    {socials.map(({id, bg, link, icon, text}) => (
+                        <li key={id} style={{backgroundColor: bg}}>
+                            <a
+                            href={link}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            title={text}
+                            >
+                                <img src={icon} alt={text} className='size-5' />
+                                <p>{text}</p>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        </>
+    )
 }
 
-export default Contact
+const ContactWindow = WindowWrapper(Contact, "contact");
+
+export default ContactWindow
